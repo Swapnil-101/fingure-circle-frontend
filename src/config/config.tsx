db68183d0775f9c1f7fb528c0937a52e4205f537
Swapnil-101 <swapniltiwari9819@gmail.com>
2023-12-08 11:19:38 +0530

    dasda

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,4 +1,4 @@
-// const baseURL = "https://fingure-circle.onrender.com";
-const baseURL = "http://localhost:5000";
+const baseURL = "https://fingure-circle.onrender.com";
+// const baseURL = "http://localhost:5000";
 
 export default baseURL;
