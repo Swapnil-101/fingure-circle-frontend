1292fb6bbe26a74acd6a3715ea88d9e8c97910ad
Swapnil-101 <74921288+Swapnil-101@users.noreply.github.com>
2024-02-12 15:37:09 +0530

    frontend chnage added new future

--- src/components/becomementor/BecomeFrom.tsx
+++ src/components/becomementor/BecomeFrom.tsx
@@ -9,7 +9,7 @@ const BecomeForm: React.FC<AppointmentFormProps> = () => {
     const [selectedOption2, setSelectedOption2] = useState('');
     const [message, setMessage] = useState('');
 
-    console.log(setSelectedOption2,setSelectedOption1)
+    console.log(setSelectedOption2, setSelectedOption1)
 
     const handleFormSubmit = () => {
         // Handle form submission logic here
@@ -25,15 +25,25 @@ const BecomeForm: React.FC<AppointmentFormProps> = () => {
                     {/* <label className="text-gray-700 dark:text-gray-200" htmlFor="username">
                         First Name
                     </label> */}
-                    <textarea
-                        name="textarea"
-                        id="text"
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        cols={30}
-                        rows={10}
-                        className="mb-10 h-40 w-full resize-none rounded-md border border-slate-300 p-5 font-semibold text-gray-300"
-                        placeholder="Description of work"
+
+                </div>
+                <div className=" my-6 flex gap-4">
+
+                    <input
+                        type="text"
+                        name="name"
+                        value={fullName}
+                        onChange={(e) => setFullName(e.target.value)}
+                        className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+                        placeholder="Expertise in Industry"
+                    />
+                    <input
+                        type="text"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+                        placeholder="What did you do"
                     />
                 </div>
                 <div className="flex gap-4">
@@ -76,6 +86,16 @@ const BecomeForm: React.FC<AppointmentFormProps> = () => {
 
                 </div>
 
+                <textarea
+                    name="textarea"
+                    id="text"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    cols={30}
+                    rows={10}
+                    className="mb-10 h-40 w-full resize-none rounded-md border border-slate-300 p-5 font-semibold text-gray-300"
+                    placeholder="Description of work"
+                />
                 <div className="text-center">
                     <button
                         className="cursor-pointer rounded-lg bg-blue-700 px-8 py-5 text-sm font-semibold text-white"
