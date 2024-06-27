81a6720fd3347da124d609d54a83737e2f0c710a
Swapnil-101 <74921288+Swapnil-101@users.noreply.github.com>
2024-06-27 13:36:58 +0530

    reecommend

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,7 +1,7 @@
 // const baseURL = "https://fingure-circle.onrender.com";
 // const baseURL = "https://swapnil-101-course-recommend.hf.space";
-const baseURL = "https://figurecircle-mainbackend.onrender.com"
-// const baseURL = "http://127.0.0.1:5000"
+// const baseURL = "https://figurecircle-mainbackend.onrender.com"
+const baseURL = "http://127.0.0.1:5000"
 
 // const baseURL = "http://localhost:5000";
 
