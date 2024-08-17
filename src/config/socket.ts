b1567171f3646ec5fb75998f9360deae713b2cc8
Swapnil-101 <74921288+Swapnil-101@users.noreply.github.com>
2024-08-17 14:06:50 +0530

    added backend server url to scoket app

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 
 
 // const socket = io("http://127.0.0.1:5000/");
-const socket = io("https://figurecircle-mainbackend.onrender.com/");
+const socket = io("http://128.199.18.137");
 
 
 
