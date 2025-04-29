69115a8002c82d3b616de5dc8594afb440f4bd71
Swapnil-101 <swapniltiwari9819@gmail.com>
2025-04-29 21:22:39 +0530

    chore:added the timezone logic on meetings compoennt

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,10 +1,10 @@
 // const baseURL = "https://fingure-circle.onrender.com";
 // const baseURL = "https://harsh1993-model.hf.space";
 // const baseURL = "https://figurecircle-mainbackend.onrender.com"
-const baseURL = "http://127.0.0.1:5000"
+// const baseURL = "http://127.0.0.1:5000"
 
 // const baseURL = "http://128.199.18.137";
-// const baseURL = "https://figurecircle.com/api";
+const baseURL = "https://figurecircle.com/api";
 
 
 // const baseURL = "http://localhost:5000";
